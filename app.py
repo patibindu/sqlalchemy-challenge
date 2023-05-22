1399bc90c3578c0c8f38aafb0ed4e3b966855872
patibindu <himabindu.pati@outlook.com>
2023-05-22 00:08:50 -0400

    upate templates

--- app.py
+++ app.py
@@ -54,13 +54,13 @@ def home():
         f"Available Routes: <br>"
         f"/api/v1.0/Precipitation<br>"
         f"/api/v1.0/stations<br>"
+        f"/api/v1.0/tob<br>"
         f"/api/v1.0/<start><br>"
         f"/api/v1.0/<start>/<end><br>"
         f"note: start and end must be dates in YYYYMMDD format with"
     )
 
 # Precipitation Route
-
 @app.route("/api/v1.0/Precipitation")
 def prcp_output():
     """Return Precipitation for the last 12 months in json format"""
@@ -94,14 +94,33 @@ def station_output():
     all_stations = list (np.ravel(all_stations_results))
 
     #return jsonify version of 
+    return jsonify(all_stations)
 
+# Temperature Route
+@app.route("/api/v1.0/tob")
+def temp_output():
+    """ return temp's for the last year for most active station"""
+    # Calculate the date one year from the last date in data set.
+    year_ago = dt.datetime(2017,8,23) - dt.timedelta(days=365)
 
-#
-@app.route("/api/v1.0/stations")
+    # Query the last 12 months of temperature observation data for this station and plot the results as a histogram
+    most_active_station_query = session.query(Measurement.tobs).\
+                            filter(Measurement.station == 'USC00519281').\
+                            filter(Measurement.date >= year_ago).all()
 
+    # Close Session
+    session.close()
 
-#
-@app.route("/api/v1.0/stations")
+    #unravel results
+    temperatures = list(np.ravel(most_active_station_query))
+
+    # return array of temp for last year for station USC00519281
+    return jsonify(temperatures = temperatures)
+
+    # start and start end routes for temp's for USC00519281
+   # @app.route("/api/v1.0/<start>")
+   # def temp start():
+   #     """ """
 
 
 
